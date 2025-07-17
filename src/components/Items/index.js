5fecd7b49b12b3d0cfb903048531fdc6e2b33fc6
OscarTinajero117 <tinajero117.oscar@gmail.com>
2025-07-17 11:28:44 -0600

    se crean elementos de ayuda repetitivos

---
+++ src/components/Items/index.js
@@ -0,0 +1,5 @@
+export { default as SeleccionOrganismo } from "@site/src/components/Items/seleccion-organismo.mdx";
+export { default as InputRFC } from "@site/src/components/Items/input-rfc.mdx";
+export { default as DatosFiscales } from "@site/src/components/Items/datos-fiscales.mdx";
+export { default as DatosContactos } from "@site/src/components/Items/datos-contactos.mdx";
+export { default as InputFolios } from "@site/src/components/Items/input-folios.mdx";
